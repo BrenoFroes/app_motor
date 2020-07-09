a65f627dcefedd7a5d78481f39435a04f8cb829d
Breno Alves Froes Peres <breno.peres@conectaservicos.com.br>
2020-07-09 12:36:19 -0300

    Alterando barra de pesquisa do veículo

--- lib/vehicle/search_vehicle_page.dart
+++ lib/vehicle/search_vehicle_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_motor/style.dart';
 import 'package:app_motor/survey/survey_register_page.dart';
 import 'package:app_motor/vehicle/vehicle_bloc.dart';
 import 'package:app_motor/vehicle/vehicle_register_page.dart';
@@ -12,94 +13,127 @@ class _SearchVehicleState extends State<SearchVehicle> {
   var bloc = new VehicleBloc();
   var response;
   bool is_Visible = false;
-  var modelo = "";
-  var placa = "";
+  var _modelo = "";
+  var _placa = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          title: TextFormField(
+            keyboardType: TextInputType.text,
+            controller: bloc.plateCtrl,
+            decoration: InputDecoration(
+              labelText: "Buscar placa:",
+              labelStyle: TextStyle(
+                color: Gray5,
+                fontSize: 20.0,
+                fontFamily: FontNameDefaultTitle,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            style: const TextStyle(
+                color: Gray5,
+                fontSize: 20.0,
+                fontFamily: FontNameDefaultTitle,
+                fontWeight: FontWeight.w600),
+            validator: (String value) {
+              if (value.length != 7)
+                return "Placa inválida.";
+              else if (value == null)
+                return "Por favor, insira uma placa.";
+              else
+                return null;
+            },
+          ),
+          backgroundColor: PrimaryBlue1,
           actions: <Widget>[
-            SizedBox(
-                width: 200,
-                child: TextFormField(
-                    controller: bloc.plateCtrl,
-                    decoration: InputDecoration(
-                      hintText: 'Buscar',
-                      hintStyle: const TextStyle(color: Colors.white30),
-                    ),
-                    style: const TextStyle(color: Colors.white, fontSize: 16.0),
-                    validator: (String value) {
-                      if (value.length != 7)
-                        return "Placa não valida";
-                      else
-                        return null;
-                    })),
             Builder(
-                builder: (context) => IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: () async {
-                        var plate = bloc.plateCtrl.text;
-                        response = await bloc.getVehicles(plate);
-                        print(response);
-                        if (response == null) {
-                          final message = SnackBar(
-                              content: Text(
-                                  "Não foi possivel encontrar um veiculo com essa placa"));
-                          Scaffold.of(context).showSnackBar(message);
-                          setState(() {
-                            is_Visible = true;
-                          });
-                        } else {
-                          final message =
-                              SnackBar(content: Text("Carro  cadastrado"));
-                          Scaffold.of(context).showSnackBar(message);
-                          setState(() {
-                            is_Visible = false;
-                            modelo = response["model"];
-                            placa = response["plate"];
-                          });
-                        }
+              builder: (context) => IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () async {
+                  var plate = bloc.plateCtrl.text;
+                  var response;
+                  response = await bloc.getVehicles(plate);
+                  if (response == null) {
+                    final message = SnackBar(
+                      content: Text(
+                          "Não foi possivel encontrar um veiculo com a placa $plate. Tente novamente"),
+                    );
+                    Scaffold.of(context).showSnackBar(message);
+                    setState(
+                      () {
+                        is_Visible = true;
                       },
-                    )),
+                    );
+                  } else {
+                    final message = SnackBar(
+                      content: Text("Carro encontrado."),
+                    );
+                    Scaffold.of(context).showSnackBar(message);
+                    setState(() {
+                      is_Visible = false;
+                      _modelo = response["model"];
+                      _placa = response["plate"];
+                    });
+                  }
+                },
+              ),
+            ),
           ],
         ),
         body: ListView(
           children: <Widget>[
             Visibility(
-                visible: !is_Visible,
-                child: ListTile(
-                  title: Text(modelo),
-                  subtitle: Text(placa),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              SurveyPage(plate: response["plate"])),
-                    );
-                  },
-                )),
+              visible: !is_Visible,
+              child: ListTile(
+                title: Text(_modelo),
+                subtitle: Text(_placa),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            SurveyPage(plate: response["plate"])),
+                  );
+                },
+              ),
+            ),
             Visibility(
-                visible: is_Visible,
-                child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 200),
-                    child: Center(
-                        child: FlatButton(
-                            color: Theme.of(context).primaryColor,
-                            child: Text("Cadastrar",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20)),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => VehicleRegisterPage(
-                                        plate: bloc.plateCtrl.text)),
-                              );
-                            }))))
+              visible: is_Visible,
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Text("Sem resultados."), 
+                    FlatButton(
+                      color: Theme.of(context).primaryColor,
+                      child: Text("Cadastrar",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => VehicleRegisterPage(
+                                  plate: bloc.plateCtrl.text)),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Visibility(
+              visible: is_Visible,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 200),
+                child: Center(
+                  child: 
+                ),
+              ),
+            ),
           ],
         ));
   }
